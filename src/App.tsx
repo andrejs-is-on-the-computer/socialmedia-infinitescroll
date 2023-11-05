fc3275513e5bc2052f00d77e9375ee35eaab1616
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-11-05 20:51:59 +0100

    shadcn added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './globals.css';
+
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import { Home } from './_root/pages';
+
+import './globals.css';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 export const App = () => {
   return (
     <main className='flex h-screen'>
         <Routes>
             {/* Public routes */}
-                <Route path='/sign-in' element={<SigninForm />} />
-                <Route path='/sign-up' element={<SignupForm />} />
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupForm />} />
+                </Route>
 
             {/* Private routes */}
-            <Route index element={<Home />} />
+            <Route element={<RootLayout />}>
+                <Route index element={<Home />} />
+            </Route>
         </Routes>
     </main>
   )
